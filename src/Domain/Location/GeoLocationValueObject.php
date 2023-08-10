c0cd1ebef694a33fbf5af6fa64164b2fd2d6b676
Adrián García <akrian231@gmail.com>
2023-08-10 15:42:59 +0200

    feat: add GeoLocation Value Object

---
+++ src/Domain/Location/GeoLocationValueObject.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Adrigar94\ValueObjectCraft\Domain\Location;
+
+use Adrigar94\ValueObjectCraft\ValueObject;
+use InvalidArgumentException;
+
+class GeoLocationValueObject implements ValueObject
+{
+
+    public function __construct(private CoordsValueObject $coords, private PlaceLocationValueObject $location)
+    {
+    }
+
+    public function getCoords(): CoordsValueObject
+    {
+        return $this->coords;
+    }
+
+    public function getLocation(): PlaceLocationValueObject
+    {
+        return $this->location;
+    }
+
+    public function isSame(ValueObject $object): bool
+    {
+        if (!$object instanceof GeoLocationValueObject) {
+            return false;
+        }
+
+        return $this->coords->isSame($object->getCoords()) && $this->location->isSame($object->getLocation());
+    }
+
+    public static function fromNative($native)
+    {
+        if (!is_array($native)) {
+            throw new InvalidArgumentException('Invalid native data provided.');
+        }
+
+        if (!isset($native['coords'], $native['location'])) {
+            throw new InvalidArgumentException('Coords and location values are required.');
+        }
+
+        $coords = CoordsValueObject::fromNative($native['coords']);
+        $location = PlaceLocationValueObject::fromNative($native['location']);
+
+        return new static($coords, $location);
+    }
+
+    public function toNative()
+    {
+        return [
+            'coords' => $this->coords->toNative(),
+            'location' => $this->location->toNative(),
+        ];
+    }
+
+    public function __toString()
+    {
+        return $this->location->__toString() . ' (' . $this->coords->__toString() . ')';
+    }
+
+    public function jsonSerialize()
+    {
+        return $this->toNative();
+    }
+}
